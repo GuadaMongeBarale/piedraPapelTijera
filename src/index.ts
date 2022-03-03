c1a5558eaf118bdc93b431cc04c297a984ed9feb
Daniel Ponce <dmep17@gmail.com>
2022-03-03 00:38:16 -0500

    Add: Estilo al index.html

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import "./assets/styles/main.css";
 import PiedraPapelTijera from "./assets/scripts/PiedraPapelTijera";
 
+// Elemnts
+const play = <HTMLButtonElement>document.getElementById("play");
+const welcomeContainer = <HTMLElement>document.getElementById("container");
+const gameConntainer = <HTMLElement>document.getElementById("game");
+
 const game = new PiedraPapelTijera({
   finished: (result) => {},
+});
+
+play.addEventListener("click", () => {
+  welcomeContainer.style.display = "none";
+  gameConntainer.style.display = "block";
 });
